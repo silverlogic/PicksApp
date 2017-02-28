83484f4602a61c6ada96f6344cb7e05c619a3d77
Emanuel  Guerrero <eg@tsl.io>
2017-02-28 11:56:14 -0500

    Initial Commit

---
+++ BaseAppV2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BaseAppV2
+//
+//  Created by Emanuel  Guerrero on 2/28/17.
+//  Copyright © 2017 SilverLogic. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
