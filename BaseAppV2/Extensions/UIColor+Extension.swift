48cd637345c3d455a28f272c407c884d685eb752
Emanuel  Guerrero <eg@tsl.io>
2017-05-09 12:16:32 -0400

    Added a base label.

--- BaseAppV2/Extensions/UIColor+Extension.swift
+++ BaseAppV2/Extensions/UIColor+Extension.swift
@@ -14,15 +14,18 @@ fileprivate extension UIColor {
     /**
         Gets the color from a given hexidecimal value.
      
-        - Parameter hexValue: An `UInt` representing the hexidecimal value
+        - Parameters:
+            - hexValue: An `UInt` representing the hexidecimal value.
+            - alpha: A `Float` representing how much alpha to apply.
+                     Default value is 1.0.
      
         - Returns: An `UIColor` object containing the color generated from `hexValue`.
     */
-    fileprivate static func colorFromHexValue(_ hexValue: UInt) -> UIColor {
+    fileprivate static func colorFromHexValue(_ hexValue: UInt, alpha: Float = 1.0) -> UIColor {
         let redValue = ((Float)((hexValue & 0xFF0000) >> 16)) / 255.0
         let greenValue = ((Float)((hexValue & 0xFF00) >> 8)) / 255.0
         let blueValue = ((Float)(hexValue & 0xFF)) / 255.0
-        return UIColor(colorLiteralRed: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+        return UIColor(colorLiteralRed: redValue, green: greenValue, blue: blueValue, alpha: alpha)
     }
 }
 
@@ -35,6 +38,9 @@ extension UIColor {
     
     /// Secondary color used in the application.
     static var secondary: UIColor { return colorFromHexValue(secondaryHexValue) }
+    
+    /// Teritary color used in the application.
+    static var teritary: UIColor { return colorFromHexValue(teritaryHexValue) }
 }
 
 
@@ -42,4 +48,5 @@ extension UIColor {
 extension UIColor {
     @nonobjc static var mainHexValue: UInt = 0x3D8DD4
     @nonobjc static var secondaryHexValue: UInt = 0xFFFFFF
+    @nonobjc static var teritaryHexValue: UInt = 0xFFFFFF
 }
